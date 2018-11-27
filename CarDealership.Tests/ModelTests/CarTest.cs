35f77d8fa9dd516890d56af7048554a039e72214
Student <example@example.com>
2018-11-27 14:57:41 -0800

    First Test working, Car objects are instantiating.

---
+++ CarDealership.Tests/ModelTests/CarTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CarDealership.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace CarDealership.Tests.ModelTests
+{
+    [TestClass]
+    public class CarTest
+    {
+        [TestMethod]
+        public void DoesClassInstantiate_Instantiation_NewObj()
+        {
+            Car testCar = new Car();
+        }
+    }
+}
---
+++ CarDealership/Models/Car.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace CarDealership.Models
+{
+
+    public class Car
+    {
+        private string MakeModel;
+        private int Price;
+        private int Miles;
+
+        public Car(string makeModel, int price, int miles)
+        {
+            MakeModel = makeModel;
+            Price = price;
+            Miles = miles;
+        }
+
+        public Car ()
+        {
+            
+        }
+
+        public string GetMakeModel()
+        {
+            return MakeModel;
+        }
+
+        public int GetPrice()
+        {
+            return Price;
+        }
+
+        public int GetMiles()
+        {
+            return Miles;
+        }
+
+        public void SetPrice(int newPrice)
+        {
+            Price = newPrice;
+        }
+
+        public bool WorthBuying(int maxPrice)
+        {
+            return (Price < maxPrice);
+        }
+
+        public bool MaxMiles(int maxMiles)
+        {
+            return (Miles < maxMiles);
+        }
+    }
+}
